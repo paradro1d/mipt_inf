db05c71829e3c013233a6e6c51afc947da2e8ad3
Paradro1d <gorbunov.sm@phystech.edu>
2020-10-03 13:08:12 +0300

    Сделал рефакторинг кода.

--- lab4/img.py
+++ lab4/img.py
@@ -1,105 +1,168 @@
-# Import a library of functions called 'pygame'
 import pygame
-from math import pi
 import numpy as np
-from random import *
+import yaml
 
+#Для изменения формы и параметров фигур используйте файл figures.yml
+#Считывание figures.yml:
+figures = yaml.load(open('figures.yml'), Loader = yaml.Loader)
+branch_coordinates = figures["branch"]
+cloud_coordinates = figures["cloudlet"]
+size = figures["size"]
+BLACK = figures["BLACK"]
+WHITE = figures["WHITE"]
+color = figures["colors"]
+trunk = figures["trunk"]
 
-#base define 
-scr_w = 900
-scr_h = 400
-scr_k = 1
-
-# Initialize the game engine
 pygame.init()
-
-# Define the colors we will use in RGB format
-
-
-# Set the height and width of the screen
-size = [scr_w, scr_h]
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("PHYSTECH VILLAGE")
 
-pygame.display.set_caption("Example code for the draw module")
-
-#Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
 
-
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
-#             0           1           2           3           4            5          6             7           8 
-color = [(15,82,14),(234,46,67),(146,106,14),(180,97,16),(14,147,145),(255,0,0),(153,227,237),(14,147,37),(249,194,194),(0,0,0)]
-
-def set_back(scr, x, y, w, h, k):
-    #back
-    pygame.draw.rect(scr, color[7], [x*k//1, (y*k)//2, w*k//1, (h*k)//1]) 
-    pygame.draw.rect(scr, color[6], [x*k//1, y*k//1, w*k//1, (h*k)//2])
-    #print("background set")
+def background(surface, width, height):
+    '''
+    Функция прорисовывает задний фон картинки.
+    surface - объект pygame.Surface - поверхность картинки .
+    width, height - ширина и высота картинки
+    '''
+    pygame.draw.rect(surface, color[6], (0, 0, width, height)) 
+    pygame.draw.rect(surface, color[5], (0, 0, width, height // 2))
     
-def house(scr, x, y, w, h, k): #house
-    pygame.draw.polygon(scr, color[5], [[int((x)*k), int((y+30)*k)], [int(((2*x + 2.9 * 40) / 2)*k) , int((y)*k)],[int((x + 2.9 * 40)*k), int((y+30)*k)]], int(30*k))    
-    pygame.draw.rect(scr, color[2],    [int((x)*k), int((y+17)*k), int((2.9*40+1)*k), int((2.1*40)*k)])
-    pygame.draw.rect(scr, color[3],    [int((x + 40-1)*k) , int((y+1.2*40 -1)*k) , int((0.8*40+2)*k), int((0.7*40 +2)*k)])
-    pygame.draw.rect(scr, color[4],    [int((x + 40 )*k), int((y+1.2*40)*k)  , int((0.8*40)*k), int((0.7*40)*k)])    
+def house(surface, x, y, width, height):
+    '''
+    Функция рисует дом.
+    surface - объект pygame.Surface (поверхность, 
+    на которой требуется изобразить объект).
+    x, y - координаты верхней левой точки картинки дома.
+    width, height - ширина и высота дома в пикселях.
+    '''
+    house_surface = pygame.Surface((585, 570), pygame.SRCALPHA)
+    house_surface_scaled = pygame.Surface((width, height), pygame.SRCALPHA)
+    pygame.draw.polygon(house_surface, color[4], \
+        [(0, 150), (290, 0), (580, 150)])
+    pygame.draw.polygon(house_surface, BLACK, \
+        [(0, 150), (290, 0), (580, 150)], 5)
+    pygame.draw.rect(house_surface, color[1], (0, 150, 585, 420))
+    pygame.draw.rect(house_surface, BLACK, (0, 150, 585, 420), 5)
+    pygame.draw.rect(house_surface, color[2], (195, 300, 170, 150))
+    pygame.draw.rect(house_surface, color[3], (200, 305, 160, 140))
+    pygame.transform.smoothscale(house_surface, \
+        (width, height), house_surface_scaled)
+    surface.blit(house_surface_scaled, (x, y))
 
-def sun(scr, x=85*4, y=40, w=0, h=0, k=1, r=20, n= 20):
-    #create sun
-    c = []; a = [ np.cos((2*np.pi)*(i/n))  for i in range(n) ]; b = [ np.sin((2*np.pi)*(i/n))  for i in range(n) ];
-    aa = [ np.cos((2*np.pi)*(i/n)+(2*np.pi)*(1/60))  for i in range(n) ]; bb = [ np.sin((2*np.pi)*(i/n)+(2*np.pi)*(1/60))  for i in range(n) ];
-    for i in range(20):
-        d = []; d.append(int((a[i]*r + x)*k)); d.append(int((b[i]*r + y)*k)); 
-        dd = []; dd.append(int((a[i]*(r-1) + x)*k)); dd.append(int((b[i]*(r-1) + y)*k));
-        c.append(d); c.append(dd); 
-    #end create sun
-    pygame.draw.polygon(screen,color[8], c,1 )
-    pygame.draw.circle(screen, color[8] , [x, y], r)
+def sun(surface, x, y, r, n):
+    '''
+    Функция рисует треугольчатое солнце.
+    surface - объект pygame.Surface (поверхность, 
+    на которой требуется изобразить объект).
+    x, y - координаты центра солнца.
+    r - радиус солнца.
+    n - натуральное число треугольников.
+    '''
+    angles = []; 
+    angles_x_even = [int(np.cos(np.pi * i / n) * r + x)  \
+        for i in range(0, 2 * n, 2)] 
+    angles_x_odd = [int(np.cos(np.pi * i / n) * 1.2 * r + x) \
+        for i in range(1, 2 * n, 2)]
+    angles_y_even = [int(np.sin(np.pi * i / n) * r + y) \
+        for i in range(0, 2 * n, 2)] 
+    angles_y_odd = [int(np.sin(np.pi * i / n) * 1.2 * r + y)  \
+        for i in range(1, 2 * n, 2)]
+    c = []
+    for i in range(n):
+        c.append((angles_x_even[i], angles_y_even[i]))
+        c.append((angles_x_odd[i], angles_y_odd[i]))
+    pygame.draw.polygon(surface, color[7], c)
+    pygame.draw.polygon(surface, BLACK, c, 1)
 
-def branch(x,y,r,k,y_bias,x_bias):
-    pygame.draw.circle(screen, color[9], [int((20+x+x_bias)*k), int(((y+12)+y_bias)*k)], int((1+r)*k));    pygame.draw.circle(screen, color[0], [int((20+x+x_bias)*k), int(((y+12)+y_bias)*k)], int(r*k));
+def branch(surface, x, y, r):
+    '''
+    Функция рисует листву дерева в виде зелёного круга.
+    surface - объект pygame.Surface (поверхность, 
+    на которой требуется изобразить объект).
+    x, y - координаты центра круга.
+    r - целое число, обозначающее радиус круга в пикселях.
+    '''
+    pygame.draw.circle(surface, BLACK, (x, y), int(1.05 * r))
+    pygame.draw.circle(surface, color[0], (x, y), r)
     
-def tree(scr, x, y, w, h, k, n): #tree
-    pygame.draw.line(screen, color[9],      [int(x*k),     int((y)*k)], [int(x*k), int((4.8 * 40-128+y)*k)], 12)
-    branch(x,y,20,k,0,0); branch(x,y,20,k,5,-15); branch(x,y,20,k,5,-25); branch(x,y,20,k,-5,-15); branch(x,y,20,k,-5,-25);     
+def tree(surface, x, y, width, height): 
+    '''
+    Функция рисует дерево.
+    surface - объект pygame.Surface (поверхность, 
+    на которой требуется изобразить объект).
+    x, y - координаты верхнего левого угла изображения дерева.
+    width, height - ширина и высота дерева в пикселях.
+    '''
+    x_surface = int(max([i[0] + 1.05 * i[2] for i in branch_coordinates]))
+    y_surface = trunk[1][1]
+    tree_surface = pygame.Surface((x_surface, y_surface), pygame.SRCALPHA)
+    tree_surface_scaled = pygame.Surface((width, height), pygame.SRCALPHA)
+    pygame.draw.line(tree_surface, BLACK, trunk[0], trunk[1], trunk[2])
+    for i in branch_coordinates:
+        branch(tree_surface, i[0], i[1], i[2])
+    pygame.transform.smoothscale(tree_surface, \
+        (width, height), tree_surface_scaled)
+    surface.blit(tree_surface_scaled, (x, y))
             
-def cloud(scr,x,y,r,k,x_bias,y_bias):
-    pygame.draw.circle(scr, (0, 0, 0), [int((x+x_bias)*k), int((y+y_bias)*k)], int((r+1)*k));    pygame.draw.circle(scr, (255, 255, 255), [int((x+x_bias)*k), int((y+y_bias)*k)], int((r)*k));    
+def cloud(surface, x, y, r):
+    '''
+    Функция рисует круглое облачко.
+    surface - объект pygame.Surface (поверхность,
+    на которой требуется изобразить объект).
+    x,y - координаты центра круга.
+    r - радиус облака.
+    '''
+    pygame.draw.circle(surface, BLACK, (x, y), int(r * 1.05))    
+    pygame.draw.circle(surface, WHITE, (x, y), r)
+    
 
-def clouds(scr, x, y, w, h, k, n):
-    cloud(scr,x,y,20,k,0,0); cloud(scr,x,y,20,k,15,0); cloud(scr,x,y,20,k,30,0); cloud(scr,x,y,20,k,45,0); cloud(scr,x,y,20,k,60,0); cloud(scr,x,y,20,k,8,12); cloud(scr,x,y,20,k,23,12); cloud(scr,x,y,20,k,38,12);
+def cloudlet(surface, x, y, width, height):
+    '''
+    Функция рисует тучу из облаков.
+    surface - объект pygame.Surface (поверхность, 
+    на которой требуется изобразить объект).
+    x, y - координаты верхнего левого угла изображения тучи.
+    width, height - ширина и высота облака в пикселях.
+    '''
+    x_surface = int(max([i[0] + 1.05 * i[2] for i in cloud_coordinates]))
+    y_surface = int(max([i[1] + 1.05 * i[2] for i in cloud_coordinates]))
+    clouds_surface = pygame.Surface((x_surface, y_surface), pygame.SRCALPHA)
+    clouds_surface_scaled = pygame.Surface((width, height), pygame.SRCALPHA)
+    for i in cloud_coordinates:
+        cloud(clouds_surface, i[0], i[1], i[2])
+    pygame.transform.smoothscale(clouds_surface, \
+        (width, height), clouds_surface_scaled)
+    surface.blit(clouds_surface_scaled, (x, y))
+
             
 while not done:
     clock.tick(1)
 
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-
-    screen.fill(WHITE)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            done = True 
+            
+    background(screen, size[0], size[1])
     
-    set_back(screen, 0, 0, scr_w, scr_h, scr_k)
-
-    #tree
-    tree(screen, 4.2*40, 128, 0, 0, 1.2, 0)
-    house(screen, 250, 136, 0, 0, 1)
+    house(screen, 250, 125, 117, 114)
     
-    tree(screen, 20*40, 188, 0, 0, 1, 0)
-    house(screen, 600, 156, 0, 0, 1)
+    tree(screen, 168, 128, 80, 107)
+    tree(screen, 780, 178, 67, 89)
+    tree(screen, 340, 178, 67, 89)
     
+    sun(screen, 340, 40, 30, 20)
     
-    tree(screen, 9*40, 188, 0, 0, 1, 0)
+    house(screen, 600, 156, 117, 114)
+    house(screen, 16, 96, 140, 137)
+    
+    cloudlet(screen, 20, 10, 102, 54)
+    cloudlet(screen, 220, 30, 102, 54)
+    cloudlet(screen, 570, 30, 153, 81)
+    cloudlet(screen, 500, 0, 102, 54)
+    cloudlet(screen, 380, 60, 102, 54)
         
-    #house
-    house(screen, 16, 96, 0, 0, 1.2)
-    
-    #sun
-    sun(screen,r=30)
-    # cloud    
-    clouds(screen, 10*4, 30, 0, 0, 1, 0); clouds(screen, 60*4, 50, 0, 0, 1, 0); clouds(screen, 200*2, 40, 0, 0, 1.5, 0); clouds(screen, 130*4, 20, 0, 0, 1, 0); clouds(screen, 100*4, 80, 0, 0, 1, 0); 
     pygame.display.flip()
 
 pygame.quit()
